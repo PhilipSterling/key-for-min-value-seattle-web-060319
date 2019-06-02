b56679d75d8ac3edd2b2153c854f8b2e9eb8b416
Philip Sterling <philipssterlings@gmail.com>
2019-06-02 08:47:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  array = []
   if hash.length = 0
     return nil
   else
     birthday_kids.collect do |key, value|
-      
+      value
     end
   end
 end
